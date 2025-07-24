4375df9042daa2115f94412595f8b04708bcaceb
Tishj <t_b@live.nl>
2025-07-24 11:36:32 +0200

    sort strings for conversion from JSON

--- src/variant_functions.cpp
+++ src/variant_functions.cpp
@@ -276,6 +276,55 @@ static optional_idx ConvertJSON(yyjson_val *val, Vector &result, VariantConversi
 	return index;
 }
 
+static void SortVariantKeys(Vector &result, VariantConversionState &state) {
+	auto &allocator = Allocator::DefaultAllocator();
+
+	auto &dictionary = ListVector::GetEntry(VariantVector::GetKeys(result));
+	idx_t dictionary_size = state.dictionary.size();
+	auto dictionary_data = FlatVector::GetData<string_t>(dictionary);
+
+	//! string + unsorted_index
+	vector<std::pair<reference<string_t>, idx_t>> strings;
+	strings.reserve(dictionary_size);
+	for (idx_t i = 0; i < dictionary_size; i++) {
+		strings.emplace_back(dictionary_data[i], i);
+	}
+
+	//! Sort the unique strings
+	std::sort(strings.begin(), strings.end(), [](const std::pair<reference<string_t>, idx_t> &a, const std::pair<reference<string_t>, idx_t> &b) {
+		return a.first.get() < b.first.get();
+	});
+
+	bool is_already_sorted = true;
+	vector<idx_t> unsorted_to_sorted(strings.size());
+	for (idx_t i = 0; i < strings.size(); i++) {
+		if (i != strings[i].second) {
+			is_already_sorted = false;
+		}
+		unsorted_to_sorted[strings[i].second] = i;
+	}
+
+	if (is_already_sorted) {
+		return;
+	}
+
+	//! Adjust the selection vector to point to the right dictionary index
+	for (idx_t i = 0; i < state.keys_count; i++) {
+		auto old_dictionary_index = state.sel_vec.get_index(i);
+		auto new_dictionary_index = unsorted_to_sorted[old_dictionary_index];
+		state.sel_vec.set_index(i, new_dictionary_index);
+	}
+
+	//! Finally, rewrite the dictionary itself
+	auto copied_dictionary = allocator.Allocate(sizeof(string_t) * dictionary_size);
+	auto copied_dictionary_data = reinterpret_cast<string_t *>(copied_dictionary.get());
+	memcpy(copied_dictionary_data, dictionary_data, sizeof(string_t) * dictionary_size);
+
+	for (idx_t i = 0; i < dictionary_size; i++) {
+		dictionary_data[i] = copied_dictionary_data[strings[i].second];
+	}
+}
+
 bool VariantFunctions::CastJSONToVARIANT(Vector &source, Vector &result, idx_t count, CastParameters &parameters) {
 	UnifiedVectorFormat source_format;
 	source.ToUnifiedFormat(count, source_format);
@@ -344,6 +393,7 @@ bool VariantFunctions::CastJSONToVARIANT(Vector &source, Vector &result, idx_t c
 		state.Reset();
 	}
 
+	SortVariantKeys(result, state);
 	auto &keys_entry = ListVector::GetEntry(keys);
 	keys_entry.Slice(state.sel_vec, state.keys_count);
 
